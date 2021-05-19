ec6c825de8281576c02edc7f5e002a6b3784258e
Erik Funke <erik.funke@christmann.info>
2021-05-19 10:50:26 +0200

    Split binarys into COSIO v1/v2

--- movebinary.py
+++ movebinary.py
@@ -2,19 +2,22 @@ Import("env", "projenv")
 from shutil import copyfile
 import os
 import zipfile
+import json
+
+versionPath = "v1"
 
 def move_firmware(source, target, env):
     print("Copying firmware binary to new location /binarys")
-    copyfile(".pio/build/d1_mini/firmware.bin", 'binarys/firmware.bin')
+    copyfile(".pio/build/d1_mini/firmware.bin", 'binarys/' + versionPath + '/firmware.bin')
     print("Done")
 
 def move_filesystem(source, target, env):
     print("Copying spiffs binary to new location /binarys")
-    copyfile(".pio/build/d1_mini/spiffs.bin", 'binarys/spiffs.bin')
+    copyfile(".pio/build/d1_mini/spiffs.bin", 'binarys/' + versionPath + '/spiffs.bin')
     
     print("Zipping /data folder")
 
-    zf = zipfile.ZipFile("binarys/data.zip", "w")
+    zf = zipfile.ZipFile("binarys/" + versionPath + "/data.zip", "w")
     for dirname, subdirs, files in os.walk("data/"):
         for filename in files:
             zf.write(os.path.join(dirname, filename))
@@ -22,6 +25,12 @@ def move_filesystem(source, target, env):
 
     print("Done")
 
+
+with open('data/config.json') as f:
+  config = json.load(f)
+
+if config["sensor"]["type"] == 3 :
+    versionPath = "v2"
 
 env.AddPostAction(".pio/build/d1_mini/firmware.bin", move_firmware)
 env.AddPostAction(".pio/build/d1_mini/spiffs.bin", move_filesystem)
